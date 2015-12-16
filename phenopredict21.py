0701ec1adc402a167c1d1748752383214726e1a4
Joe Pickrell <joepickrell@gmail.com>
2015-12-16 04:33:46 +0000

    read model and vcf file

--- phenopredict21.py
+++ phenopredict21.py
@@ -1,4 +1,4 @@
-import sys
+import sys, gzip
 import json
 import click
 
@@ -16,20 +16,75 @@ requests = BitTransferRequests(wallet, username)
 server_url = 'http://localhost:5000/'
 
 
+def listsnps(phenomodel):
+	toreturn = dict()
+	allsnps = phenomodel['snps']
+	for snplist in allsnps:
+		for snp in snplist:
+			# Using chr:pos as unique identifier. Dangerous but good enough for now. 
+			id = snp['chr'][3:]+":"+str(snp['pos'])
+			toreturn[id] = snp
+	return toreturn			
+
+def readVCF(vcffile, snps):
+	infile = gzip.open(vcffile)
+	toreturn = dict()
+	line = infile.readline()
+	while line:
+		line = bytes.decode(line)
+		line = line.strip().split()
+		if line[0][0] == "#":
+			line = infile.readline()
+			continue
+		# Using chr:pos as unique identifier
+		id = line[0]+":"+line[1]
+		if line[2] == "rs1752684":
+			print(line) 
+		if id in snps: 
+			print(line)
+			ref = line[3]
+			alt = line[4]
+			gtid = -9
+			format = line[8].split(":")
+			for i in range(len(format)):
+				if format[i] == "GT":
+					gtid = i
+			if gtid == -9:
+				print("Bad VCF")
+				return 1
+			geno = line[9].split(":")[gtid]
+			a1 = ref
+			if geno[0] == "1": a1 = alt
+			a2 = ref
+			if geno[2] == "1": a2 = alt
+			toreturn[id] = {'a1': a1, 'a2': a2}
+		line = infile.readline()
+	return toreturn
+		
+def predict(phenomodel, vcffile):
+	allsnps = listsnps(phenomodel)
+	print(allsnps)
+	vcfsnps = readVCF(vcffile, allsnps)
+	print(vcfsnps)
+	return
+
 @click.command()
 @click.option('--pheno', help = 'Phenotype to predict')
+@click.option('--phenos', is_flag=True, help = 'List phenotypes available')
 @click.option('--vcf', help = 'input VCF file')
-def cli(pheno, vcf):
+def cli(pheno, phenos, vcf):
 
-    sel_url = server_url+'phenos'
-    response = requests.get(url=sel_url)
-    phenos = json.loads(response.text)
-    click.echo(phenos)
-    if pheno in phenos['phenolist']:
-        url2 = server_url+'phenos/'+pheno
-        response = requests.get(url=url2)
-        model = json.loads(response.text)
-        click.echo(model)
-    else:
-        coords = json.loads(response.text)
-        click.echo(coords)
+	sel_url = server_url+'phenos'
+	response = requests.get(url=sel_url)
+	ps = json.loads(response.text)
+	if phenos:
+		click.echo(json.dumps(ps))
+		return
+ 
+	if pheno in ps['phenolist']:
+        	url2 = server_url+'phenos/'+pheno
+        	response = requests.get(url=url2)
+        	model = json.loads(response.text)
+        	predict(model, vcf)
+	else:
+		click.echo('no phenotype: '+pheno)
